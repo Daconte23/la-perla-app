547bd6868741f142c94bfd71d90b23cbecf82a61
Aquiles Cohen <aqcohen@gmail.com>
2022-12-16 10:47:53 -0500

    Update Banner

--- includes/form_1.php
+++ includes/form_1.php
@@ -6,7 +6,7 @@
 	
 	$name = $_POST['name'];
 	$email = $_POST['email'];
-	$input_2095 = $_POST['input_2095'];
+	$number = $_POST['number'];
 	$message = $_POST['message'];
 	$optin = $_POST['optin'];
 	
@@ -15,7 +15,7 @@
 	// Create email	
 	$email_subject = "Duda desde el formulario de cotacto.";
 	$email_body = "Ud ha recibido un nuevo mensaje \n\n".
-				  "Name: $name \nEmail: $email \nInput_2095: $input_2095 \nMessage: $message \nOptin: $optin \n";
+				  "Name: $name \nEmail: $email \nNumber: $number \nMessage: $message \nOptin: $optin \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
 	$headers .= "From: convocatorias@laperlaapp.com\r\n";
 	$headers .= "Reply-To: $email";	
