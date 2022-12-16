aa46fe5ef3ea0561e8a314a9e29f5666560e62dc
Aquiles Cohen <aqcohen@gmail.com>
2022-12-16 08:51:19 -0500

    Actualizar landing

---
+++ includes/form_1.php
@@ -0,0 +1,25 @@
+<?php	
+	if (empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['message']) && strlen($_POST['message']) == 0)
+	{
+		return false;
+	}
+	
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$input_2095 = $_POST['input_2095'];
+	$message = $_POST['message'];
+	$optin = $_POST['optin'];
+	
+	$to = 'aqcohen@gmail.com'; // Email submissions are sent to this email
+
+	// Create email	
+	$email_subject = "Duda desde el formulario de cotacto.";
+	$email_body = "Ud ha recibido un nuevo mensaje \n\n".
+				  "Name: $name \nEmail: $email \nInput_2095: $input_2095 \nMessage: $message \nOptin: $optin \n";
+	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
+	$headers .= "From: convocatorias@laperlaapp.com\r\n";
+	$headers .= "Reply-To: $email";	
+	
+	mail($to,$email_subject,$email_body,$headers); // Post message
+	return true;			
+?>
